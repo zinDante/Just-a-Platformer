3609ac8bd3326d1e437d5962e0c7993b98ace005
Debasish Mohapatra <93515354+zinDante@users.noreply.github.com>
2022-11-11 18:33:30 +0530

    Add files via upload
    Scripts

---
+++ GameplayUIController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameplayUIController : MonoBehaviour
+{
+    
+    public void RestartGame() {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void HomeButton() {
+        SceneManager.LoadScene("Mainmenu");
+    }
+}
